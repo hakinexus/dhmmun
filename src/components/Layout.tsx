0e8a049b4115fcc5576b12ffa7f35bc084b390f0
nexus <husseinrabihhijazi@gmail.com>
2026-06-06 20:16:07 +0400

    removed scroll bar laks

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import ScrollProgress from './ScrollProgress';
 
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col selection:bg-primary selection:text-on-primary overflow-x-hidden w-full">
-      <ScrollProgress />
       <Navbar />
       <div className="flex-grow flex flex-col">
         <Outlet />
